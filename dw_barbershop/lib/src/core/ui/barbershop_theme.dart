1cc7915f04a16bf3616496f6301320a40b7ca750
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2023-08-15 12:42:15 -0300

    Dia 1

---
+++ dw_barbershop/lib/src/core/ui/barbershop_theme.dart
@@ -0,0 +1,36 @@
+import 'package:dw_barbershop/src/core/ui/constrants.dart';
+import 'package:flutter/material.dart';
+
+sealed class BarbershopTheme {
+  static const _defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(8),
+    ),
+    borderSide: BorderSide(color: ColorsConstrants.grey),
+  );
+
+  static ThemeData themeData = ThemeData(
+    useMaterial3: true,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.white,
+      labelStyle: const TextStyle(color: ColorsConstrants.grey),
+      border: _defaultInputBorder,
+      enabledBorder: _defaultInputBorder,
+      focusedBorder: _defaultInputBorder,
+      errorBorder: _defaultInputBorder.copyWith(
+        borderSide: const BorderSide(color: ColorsConstrants.red),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: ColorsConstrants.brown,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+    fontFamily: FontConstrants.fontFamily,
+  );
+}
