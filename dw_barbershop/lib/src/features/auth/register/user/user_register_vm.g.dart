c4df5a120d5b9a9c6f0e0c8b8c74ce7f567d4557
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2023-08-16 16:42:48 -0300

    Dia 3

---
+++ dw_barbershop/lib/src/features/auth/register/user/user_register_vm.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_register_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userRegisterVmHash() => r'41cfc9faf7b8cc7471b33e842f68cb27106731eb';
+
+/// See also [UserRegisterVm].
+@ProviderFor(UserRegisterVm)
+final userRegisterVmProvider = AutoDisposeNotifierProvider<UserRegisterVm,
+    UserRegisterStateStatus>.internal(
+  UserRegisterVm.new,
+  name: r'userRegisterVmProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRegisterVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserRegisterVm = AutoDisposeNotifier<UserRegisterStateStatus>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
